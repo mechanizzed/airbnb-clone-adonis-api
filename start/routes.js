5d7af3c2daddfd6bf63e28557b6172fa6f9902de
mechanizzed <dsibr@hotmail.com>
2018-07-19 14:45:35 -0300

    Updated routes

--- start/routes.js
+++ start/routes.js
@@ -2,5 +2,18 @@
 
 const Route = use('Route');
 
+// Users
 Route.post('/users', 'UserController.create');
 Route.post('sessions', 'SessionController.create');
+
+// Properties
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth');
+
+// Images Properties
+// Create
+Route.post('properties/:id/images', 'ImageController.store').middleware('auth');
+
+// Get
+Route.get('images/:path', 'ImageController.show');
